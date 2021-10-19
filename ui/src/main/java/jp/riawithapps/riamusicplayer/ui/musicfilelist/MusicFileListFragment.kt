8dfc0b888593b36d50e1fbca502ade0b3eebb2b1
riawiththesam <riawiththesam@gmail.com>
2021-10-19 22:52:21 +0900

    feat: ホーム画面系のFragmentを一部仮追加

---
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/musicfilelist/MusicFileListFragment.kt
@@ -0,0 +1,7 @@
+package jp.riawithapps.riamusicplayer.ui.musicfilelist
+
+import androidx.fragment.app.Fragment
+import jp.riawithapps.riamusicplayer.ui.R
+
+class MusicFileListFragment: Fragment(R.layout.fragment_music_file_list) {
+}
---
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/search/SearchFragment.kt
@@ -0,0 +1,7 @@
+package jp.riawithapps.riamusicplayer.ui.search
+
+import androidx.fragment.app.Fragment
+import jp.riawithapps.riamusicplayer.ui.R
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+}
