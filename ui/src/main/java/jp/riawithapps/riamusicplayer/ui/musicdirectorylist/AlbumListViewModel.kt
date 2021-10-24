fafcfdb8dffe05f4c6af26abbee8b62a1b879114
riawiththesam <riawiththesam@gmail.com>
2021-10-25 03:57:27 +0900

    refactor: AlbumListViewModelを整理

--- ui/src/main/java/jp/riawithapps/riamusicplayer/ui/musicdirectorylist/AlbumListViewModel.kt
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/musicdirectorylist/AlbumListViewModel.kt
@@ -1,20 +1,16 @@
 package jp.riawithapps.riamusicplayer.ui.musicdirectorylist
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import jp.riawithapps.riamusicplayer.ui.util.createScope
 import jp.riawithapps.riamusicplayer.ui.util.emit
 import jp.riawithapps.riamusicplayer.usecase.music.MusicFile
 import jp.riawithapps.riamusicplayer.usecase.musicdirectorylist.MusicListUseCase
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.plus
 
 class AlbumListViewModel(
-    private val musicDirectoryListUseCase: MusicListUseCase,
+    private val musicListUseCase: MusicListUseCase,
 ) : ViewModel() {
-    private val handler = CoroutineExceptionHandler { _, e -> e.printStackTrace() }
-    private val scope = viewModelScope + handler
+    private val scope = createScope()
 
     private val _musicList = MutableStateFlow<List<MusicFile>>(emptyList())
     val musicList: StateFlow<List<MusicFile>> = _musicList
@@ -27,13 +23,11 @@ class AlbumListViewModel(
     }
 
     fun onClickScan() {
-        scope.launch {
-            _event.emit(AlbumListEvent.RequestPermission)
-        }
+        _event.emit(scope, AlbumListEvent.RequestPermission)
     }
 
     fun doScan() {
-        musicDirectoryListUseCase.scan()
+        musicListUseCase.scan()
             .onEach { _musicList.value = it.list }
             .launchIn(scope)
     }
