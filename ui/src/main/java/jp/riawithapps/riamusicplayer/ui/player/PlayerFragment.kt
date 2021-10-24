fe8a2a3b24dc7831956f3b9b615896997974cebf
riawiththesam <riawiththesam@gmail.com>
2021-10-25 03:10:52 +0900

    feat: プレイヤー画面仮追加

---
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/player/PlayerFragment.kt
@@ -0,0 +1,7 @@
+package jp.riawithapps.riamusicplayer.ui.player
+
+import androidx.fragment.app.Fragment
+import jp.riawithapps.riamusicplayer.ui.R
+
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+}
