6a4c401deadff7e8e298503081ba64c6bb0602c8
riawiththesam <riawiththesam@gmail.com>
2021-10-25 02:39:20 +0900

    feat: パーミッション取得と音楽情報取得を実装

--- ui/src/main/java/jp/riawithapps/riamusicplayer/ui/musicdirectorylist/MusicDirectoryListFragment.kt
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/musicdirectorylist/MusicDirectoryListFragment.kt
@@ -1,8 +1,13 @@
 package jp.riawithapps.riamusicplayer.ui.musicdirectorylist
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import jp.riawithapps.riamusicplayer.ui.R
 import jp.riawithapps.riamusicplayer.ui.databinding.FragmentMusicDirectoryListBinding
@@ -11,6 +16,14 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MusicDirectoryListFragment : Fragment(R.layout.fragment_music_directory_list) {
     private val viewModel by viewModel<MusicDirectoryListViewModel>()
+
+    private val permissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            viewModel.doScan()
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,5 +35,17 @@ class MusicDirectoryListFragment : Fragment(R.layout.fragment_music_directory_li
         viewModel.directoryList.repeatCollectOnStarted(this) {
             Log.d("test", "directoryList $it")
         }
+        viewModel.event.repeatCollectOnStarted(this) {
+            if (context?.hasReadPermission() == true) {
+                viewModel.doScan()
+            } else {
+                permissionRequest.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    private fun Context.hasReadPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
     }
 }
--- ui/src/main/java/jp/riawithapps/riamusicplayer/ui/musicdirectorylist/MusicDirectoryListViewModel.kt
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/musicdirectorylist/MusicDirectoryListViewModel.kt
@@ -2,23 +2,38 @@ package jp.riawithapps.riamusicplayer.ui.musicdirectorylist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import jp.riawithapps.riamusicplayer.usecase.musicdirectorylist.MusicDirectoryListUseCase
+import jp.riawithapps.riamusicplayer.usecase.music.MusicFile
+import jp.riawithapps.riamusicplayer.usecase.musicdirectorylist.MusicListUseCase
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
 
 class MusicDirectoryListViewModel(
-    private val musicDirectoryListUseCase: MusicDirectoryListUseCase,
+    private val musicDirectoryListUseCase: MusicListUseCase,
 ) : ViewModel() {
     private val handler = CoroutineExceptionHandler { _, e -> e.printStackTrace() }
     private val scope = viewModelScope + handler
 
-    private val _directoryList = MutableStateFlow<List<String>>(emptyList())
-    val directoryList: StateFlow<List<String>> = _directoryList
+    private val _directoryList = MutableStateFlow<List<MusicFile>>(emptyList())
+    val directoryList: StateFlow<List<MusicFile>> = _directoryList
+
+    private val _event = MutableSharedFlow<MusicDirectoryListEvent>()
+    val event: SharedFlow<MusicDirectoryListEvent> = _event
 
     fun onClickScan() {
+        scope.launch {
+            _event.emit(MusicDirectoryListEvent.RequestPermission)
+        }
+    }
+
+    fun doScan() {
         musicDirectoryListUseCase.scan()
             .onEach { _directoryList.value = it.list }
             .launchIn(scope)
     }
+}
+
+sealed class MusicDirectoryListEvent {
+    object RequestPermission : MusicDirectoryListEvent()
 }
