5b058d35aff8068b62b22b85a38054d801373c8a
riawiththesam <riawiththesam@gmail.com>
2021-10-25 03:09:57 +0900

    refactor: 不要な要素を削除

--- app/src/main/java/jp/riawithapps/riamusicplayer/UseCaseModule.kt
+++ app/src/main/java/jp/riawithapps/riamusicplayer/UseCaseModule.kt
@@ -1,12 +1,9 @@
 package jp.riawithapps.riamusicplayer
 
-import jp.riawithapps.riamusicplayer.usecase.HomeInteractor
-import jp.riawithapps.riamusicplayer.usecase.HomeUseCase
 import jp.riawithapps.riamusicplayer.usecase.musicdirectorylist.MusicDirectoryListInteractor
 import jp.riawithapps.riamusicplayer.usecase.musicdirectorylist.MusicListUseCase
 import org.koin.dsl.module
 
 fun createUseCaseModule() = module {
-    single<HomeUseCase> { HomeInteractor(get()) }
     single<MusicListUseCase> { MusicDirectoryListInteractor(get()) }
 }
--- ui/src/main/java/jp/riawithapps/riamusicplayer/ui/MainActivity.kt
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-    private val mainViewModel by viewModel<MainViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("test", mainViewModel.test())
     }
 }
