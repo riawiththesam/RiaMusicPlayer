7f7a06d9f26ff05ea68eefcc2d20ebdf78c161de
riawiththesam <riawiththesam@gmail.com>
2021-11-08 02:37:52 +0900

    refactor: MediaSessionCompatの操作を切り出し

---
+++ ui/src/main/java/jp/riawithapps/riamusicplayer/ui/util/MediaSessionCompats.kt
@@ -0,0 +1,49 @@
+package jp.riawithapps.riamusicplayer.ui.util
+
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import com.google.android.exoplayer2.ExoPlayer
+
+/**
+ * プレイヤーの状態をMediaSessionに設定
+ *
+ * @param exoPlayer
+ */
+fun MediaSessionCompat.setExoPlayerState(exoPlayer: ExoPlayer) {
+    val state = PlaybackStateCompat.Builder().apply {
+        // 取り扱う操作。とりあえず 再生準備 再生 一時停止 シーク を扱うようにする。書き忘れると何も起きない
+        setActions(
+            PlaybackStateCompat.ACTION_PREPARE
+                    or PlaybackStateCompat.ACTION_PLAY
+                    or PlaybackStateCompat.ACTION_PAUSE
+                    or PlaybackStateCompat.ACTION_STOP
+                    or PlaybackStateCompat.ACTION_SEEK_TO
+        )
+        // 再生してるか。ExoPlayerを参照
+        val state = if (exoPlayer.isPlaying) PlaybackStateCompat.STATE_PLAYING else PlaybackStateCompat.STATE_PAUSED
+        // 位置
+        val position = exoPlayer.currentPosition
+        // 再生状態を更新
+        setState(state, position, 1.0f) // 最後は再生速度
+    }.build()
+    this.setPlaybackState(state)
+}
+
+/**
+ * プレイヤーのメタデータをMediaSessionに設定
+ *
+ * @param exoPlayer
+ */
+fun MediaSessionCompat.setExoPlayerMetaData(exoPlayer: ExoPlayer) {
+    val mediaMetadataCompat = MediaMetadataCompat.Builder().apply {
+        putString(MediaMetadataCompat.METADATA_KEY_TITLE, "音楽のタイトル")
+        putString(MediaMetadataCompat.METADATA_KEY_ARTIST, "音楽のアーティスト")
+        putLong(
+            MediaMetadataCompat.METADATA_KEY_DURATION,
+            exoPlayer.duration
+        )
+    }.build()
+
+    this.setMetadata(mediaMetadataCompat)
+}
